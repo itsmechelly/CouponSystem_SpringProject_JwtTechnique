b9b11e995217af454574cb4b03ec29998f021035
Chelly izraelov <hlly.lov@gmail.com>
2021-06-21 11:35:07 +0300

    ...

--- CouponSystem_SpringProject_JwtTechnique/src/main/java/com/couponsystem/security/LoginController.java
+++ CouponSystem_SpringProject_JwtTechnique/src/main/java/com/couponsystem/security/LoginController.java
@@ -3,6 +3,7 @@ package com.couponsystem.security;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import com.couponsystem.exceptions.NotFoundException;
 import com.couponsystem.service.LoginService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
 public class LoginController {
 
 	private final LoginService loginService;
