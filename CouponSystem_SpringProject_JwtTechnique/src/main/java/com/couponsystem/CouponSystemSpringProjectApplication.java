f2e0e7996da5fee51397560d8a65640877c13f97
Chelly izraelov <hlly.lov@gmail.com>
2021-11-10 18:57:49 +0200

    component scan

--- CouponSystem_SpringProject_JwtTechnique/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
+++ CouponSystem_SpringProject_JwtTechnique/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
@@ -2,8 +2,10 @@ package com.couponsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.couponsystem.rest", "com.couponsystem.security"})
 public class CouponSystemSpringProjectApplication {
 
 	public static void main(String[] args) {
