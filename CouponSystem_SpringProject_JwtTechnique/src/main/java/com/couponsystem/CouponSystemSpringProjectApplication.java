15e96b3548152ed736da6d7d4cbf6400ec776e07
Chelly izraelov <hlly.lov@gmail.com>
2021-11-11 15:54:03 +0200

    filter bean

--- CouponSystem_SpringProject_JwtTechnique/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
+++ CouponSystem_SpringProject_JwtTechnique/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
@@ -2,13 +2,29 @@ package com.couponsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.couponsystem.filters.TokenFilter;
+import com.couponsystem.security.JwtUtil;
 
 @SpringBootApplication
+@EnableTransactionManagement
 //@ComponentScan(basePackages = {"com.couponsystem.rest", "com.couponsystem.security"})
 public class CouponSystemSpringProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CouponSystemSpringProjectApplication.class, args);
+	}
+	
+	@Bean
+	public FilterRegistrationBean<TokenFilter> tokenFilterRegistration(JwtUtil jwtUtil){
+		FilterRegistrationBean<TokenFilter> filterRegistrationBean = new FilterRegistrationBean<TokenFilter>();
+		TokenFilter tokenFilter = new TokenFilter(jwtUtil);
+		filterRegistrationBean.setFilter(tokenFilter);
+		filterRegistrationBean.addUrlPatterns("/*");
+		return filterRegistrationBean;
 	}
 }
