5ff6af933691a587d177c60d4e84c263471c09f9
Chelly izraelov <hlly.lov@gmail.com>
2021-11-11 16:06:03 +0200

    ...

--- CouponSystem_SpringProject_JwtTechnique/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
+++ CouponSystem_SpringProject_JwtTechnique/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
@@ -1,11 +1,16 @@
 package com.couponsystem;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.couponsystem.filters.TokenFilter;
 import com.couponsystem.security.JwtUtil;
@@ -18,6 +23,20 @@ public class CouponSystemSpringProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CouponSystemSpringProjectApplication.class, args);
 	}
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://couponexpress.herokuapp.com"));
+        configuration.setAllowedOrigins(Arrays.asList("https://best-coupon-system-ever-client.herokuapp.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+                "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 	
 	@Bean
 	public FilterRegistrationBean<TokenFilter> tokenFilterRegistration(JwtUtil jwtUtil){
