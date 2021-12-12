5a22d0abb2edd8c586fca6bff2734ebfb5af1a0e
Chelly izraelov <hlly.lov@gmail.com>
2021-12-12 16:49:08 +0200

    ...

--- CouponSystem_SpringProject_JwtTechnique/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
+++ CouponSystem_SpringProject_JwtTechnique/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
@@ -1,10 +1,15 @@
 package com.couponsystem;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.couponsystem.filters.TokenFilter;
 import com.couponsystem.security.JwtUtil;
@@ -25,4 +30,18 @@ public class CouponSystemSpringProjectApplication {
 		filterRegistrationBean.addUrlPatterns("/*");
 		return filterRegistrationBean;
 	}
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://couponexpress.herokuapp.com"));
+        configuration.setAllowedOrigins(Arrays.asList("https://best-coupon-system-ever-client.herokuapp.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+                "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
